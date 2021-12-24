ace2f65b538a11396f8c3966fabbd8ae22d55eef
max <mxpetrovsky@gmail.com>
2021-12-24 14:12:53 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: QuoteList(),
+  ));
+}
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuotesState createState() => _QuotesState();
+}
+
+class _QuotesState extends State<QuoteList> {
+
+  List<Quote> quotes = [
+    Quote(text: 'Все что начинается, начинается с нуля', author: 'Илюха'),
+    Quote(text: 'Расгоготался тут, гусяныч', author: 'Илюха'),
+    Quote(text: 'Только добро и осмысленность порождает добро и осмысленность. Зло порожлает только зло', author: 'Илюха'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text(
+          'Awesome Quotes'
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList(),
+        crossAxisAlignment: CrossAxisAlignment.start,
+      )
+    );
+  }
+}
